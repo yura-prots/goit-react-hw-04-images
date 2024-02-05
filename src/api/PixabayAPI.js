b3e9db9769d6cab3156afe686abf8c9de9b8172f
yura-prots <yuraprots@gmail.com>
2024-02-05 18:14:58 +0200

    remove AbortController

--- src/api/PixabayAPI.js
+++ src/api/PixabayAPI.js
@@ -1,20 +1,12 @@
 import axios from 'axios';
 
 const API_KEY = '41545079-e9b3a4168a776ff0916b92321';
-// const controller = new AbortController();
-// const CancelToken = axios.CancelToken;
-// const source = CancelToken.source();
 
 const fetchImages = async (query, page, perPage) => {
   const response = await axios({
     url: `https://pixabay.com/api/?key=${API_KEY}&q=${query}&page=${page}&image_type=photo&orientation=horizontal&per_page=${perPage}`,
     method: 'get',
-    // signal: controller.signal,
-    // cancelToken: source.token,
   });
-
-  // controller.abort();
-  // source.cancel();
 
   return response.data;
 };
